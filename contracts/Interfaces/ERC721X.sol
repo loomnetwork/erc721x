1b7a7b18fa82867d3fbc2dd857ad00250f9577e0
Georgios Konstantopoulos <me@gakonst.com>
2018-09-14 15:49:18 +0300

    Update package.json and interface

--- contracts/Interfaces/ERC721X.sol
+++ contracts/Interfaces/ERC721X.sol
@@ -4,6 +4,7 @@ pragma solidity 0.4.24;
 contract ERC721X {
   function implementsERC721X() public pure returns (bool);
   function ownerOf(uint256 _tokenId) public view returns (address _owner);
+  function balanceOf(address owner) public view returns (uint256);
   function balanceOf(address owner, uint256 tokenId) public view returns (uint256);
   function tokensOwned(address owner) public view returns (uint256[], uint256[]);
 
